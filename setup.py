2439fd5cbb2485900f1a655d205ba3cfa701cb2d
Leo Hanisch <HanischLeo@freenet.de>
2018-11-25 19:27:48 +0100

    removed version file

--- setup.py
+++ setup.py
@@ -2,18 +2,13 @@ from os import path
 
 from setuptools import find_packages, setup
 
-# pylint: disable=exec-used,undefined-variable
-
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), 'r') as rf:
     LONG_DESCRIPTION = rf.read()
-
-with open(path.join(path.abspath(path.dirname(__file__)), 'pylintfileheader/_version.py'), 'r') as f:
-    exec(f.read())
 
 setup(
     # PEP8: Packages should also have short, all-lowercase names, the use of underscores is discouraged
     name='pylintfileheader',
-    version=__version__,
+    version='0.0.1',
     packages=find_packages(exclude=['*test']),
     description='Enables pylint to force a consistent file header.',
     long_description=LONG_DESCRIPTION,
